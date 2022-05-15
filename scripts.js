b10f437bea2629b98e68fedbc0d80891d105e2ec
st-182 <31894656+st-182@users.noreply.github.com>
2022-05-15 15:46:29 +0300

    project is finished

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@
 
 const multiStepForm = document.querySelector("[data-multi-step]");
 const formSteps = [...multiStepForm.querySelectorAll("[data-step]")];
+const progressBarSteps = [...document.querySelectorAll(".step")];
 let currentStep = parseInt(
   formSteps.findIndex((step) => {
     return step.classList.contains("active");
@@ -12,22 +13,28 @@ console.log(currentStep);
 
 if (currentStep < 0) {
   currentStep = 0;
+  progressBarSteps[currentStep].classList.add("active-step");
   formSteps[currentStep].classList.add("active");
 }
 
 multiStepForm.addEventListener("click", (e) => {
   const validityCheck = () => {
     const inputs = [...formSteps[currentStep].querySelectorAll("input")];
-    const allValid = inputs.every((input) => input.reportValidity());
+    const allValid = inputs.every((input) => {
+      console.log(input.reportValidity());
+      return input.reportValidity();
+    });
     return allValid;
   };
   if (e.target.matches("[data-next]")) {
     if (!validityCheck()) return;
     currentStep++;
+    progressBarSteps[currentStep].classList.add("active-step");
   } else if (e.target.matches("[data-previous]")) {
+    progressBarSteps[currentStep].classList.remove("active-step");
     currentStep--;
   } else {
-    return console.log("wrong element");
+    return;
   }
 
   showCurrentStep();
@@ -38,7 +45,11 @@ const showCurrentStep = () => {
   //   formSteps.forEach((step) => step.classList.remove("active"));
   //   formSteps[currentStep].classList.add("active");
   //   correct one:
-  formSteps.forEach((step, index) =>
-    step.classList.toggle("active", index === currentStep)
-  );
+  formSteps.forEach((step, index) => {
+    step.classList.toggle("active", index === currentStep);
+  });
 };
+multiStepForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(multiStepForm);
+});
